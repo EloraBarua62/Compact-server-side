95a6efce5679fa3f6e187bae92d2ff9343bfd988
EloraBarua <elorabarua62@gmail.com>
2022-05-23 21:53:25 +0600

    Database connected

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// Database
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ghbw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run()
+{
+    try{
+        await client.connect();
+
+        // Database collections
+        const partsCollections = client.db('COMPACT').collection('parts');
+        
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+
 // Root API
 app.get('/', (req,res) => {
     res.send('COMPACT server is running');
